9c73786d36dc2049805fa3064d9a3d00c4797397
Dasu <dasuuu1812@gmail.com>
2025-08-19 10:09:43 +0530

    view user error solve

--- resources/views/users/create_user.blade.php
+++ resources/views/users/create_user.blade.php
@@ -10,10 +10,6 @@
                 <form action="{{ isset($user) ? route('users.update', $user->id) : route('users.store') }}" method="post"
                     enctype="multipart/form-data" id="user-form">
                     @csrf
-                    @if (isset($user))
-                        @method('PUT')
-                    @endif
-
                     <div class="mb-3 form-group">
                         <label for="name" class="form-label">Name</label>
                         <input type="text" class="form-control" id="name" name="name"
