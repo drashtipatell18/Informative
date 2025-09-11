293b93aafc2bae39c9ede5138c45692b44f828d5
Dasu <drashtii.kalathiyainfotech@gmail.com>
2025-09-11 14:38:21 +0530

    admin template add logo set done

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -136,11 +136,9 @@
         <div class="sidenav-header">
             <i class="fas fa-times p-3 cursor-pointer text-secondary opacity-5 position-absolute end-0 top-0 d-none d-xl-none"
                 aria-hidden="true" id="iconSidenav"></i>
-            <a class="navbar-brand m-0" href=" https://demos.creative-tim.com/argon-dashboard/pages/dashboard.html "
-                target="_blank">
-                <img src="{{ asset('assets/img/logo-ct-dark.png') }}" width="26px" height="26px"
-                    class="navbar-brand-img h-100" alt="main_logo">
-                <span class="ms-1 font-weight-bold">Creative Tim</span>
+            <a class="navbar-brand m-0" href="{{ route('index') }}">
+                <img src="{{ asset('assets/img/logo__2.png') }}" alt="Medical Stock Logo" class="logo-img"
+                    style="width: 100%; height: 50px; object-fit: contain;">
             </a>
         </div>
         <hr class="horizontal dark mt-0">
@@ -277,8 +275,7 @@
                 </li>
 
                 <li class="nav-item">
-                    <a class="nav-link {{ Request::is('aboutus*') ? 'active' : '' }}"
-                        href="{{ route('aboutus') }}">
+                    <a class="nav-link {{ Request::is('aboutus*') ? 'active' : '' }}" href="{{ route('aboutus') }}">
                         <div
                             class="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center">
                             <i class="ni ni-image text-dark text-sm opacity-10"></i>
